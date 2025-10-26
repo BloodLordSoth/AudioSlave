dbf63e61d7cae43697d6fa81e6fdcc1ecfd3d723
Lord Soth <allhailsoth@gmail.com>
2025-10-26 04:50:11 -0400

    pushing cd for delivery

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,3 +7,14 @@ describe('/GET dashboard', () => {
         expect(res.statusCode).toBe(200)
     })
 })
+
+describe('/POST register endpoint', () => {
+    describe('Without username', () => {
+        test('Should return 401 statusCode', async () => {
+            const res = await request(app).post('/register').send({
+                password: "heythere"
+            })
+            expect(res.statusCode).toBe(401)
+        })
+    })
+})
